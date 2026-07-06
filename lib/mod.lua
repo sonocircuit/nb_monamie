4bbdff91b7737ae5febabbeecc07abed75be99be
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2026-07-06 18:14:00 +0200

    rename and fixes

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,13 +1,15 @@
--- mes amis - an approximation of just friends synth mode v.1.0 @sonoCircuit
+-- monami.e - an approximation of just friends synth mode v.1.0 @sonoCircuit
 -- based on ImaginaryFriends @synthetivv
 -- thank you for your amazing waveshaping implementation!
 
+local fs = require 'fileselect'
+local tx = require 'textentry'
 local mu = require 'musicutil'
 local md = require 'core/mods'
 local vx = require 'voice'
 
-local preset_path = "/home/we/dust/data/nb_mesamis/mesamis_patches"
-local default_patch = "/home/we/dust/data/nb_mesamis/mesamis_patches/default.pma"
+local preset_path = "/home/we/dust/data/nb_monamie/monamie_patches"
+local default_patch = "/home/we/dust/data/nb_monamie/monamie_patches/default.pam"
 local current_patch = ""
 local is_active = false
 
@@ -22,28 +24,28 @@ local paramlist = {
 
 ---------------- osc msgs ----------------
 
-local function init_nb_mesamis()
-  osc.send({ "localhost", 57120 }, "/nb_mesamis/init")
+local function init_nb_monamie()
+  osc.send({ "localhost", 57120 }, "/nb_monamie/init")
 end
 
-local function free_nb_mesamis()
-  osc.send({ "localhost", 57120 }, "/nb_mesamis/free")
+local function free_nb_monamie()
+  osc.send({ "localhost", 57120 }, "/nb_monamie/free")
 end
 
 local function dont_panic()
-  osc.send({ "localhost", 57120 }, "/nb_mesamis/panic")
+  osc.send({ "localhost", 57120 }, "/nb_monamie/panic")
 end
 
 local function note_on(voice, freq, vel)
-  osc.send({ "localhost", 57120 }, "/nb_mesamis/note_on", {voice, freq, vel})
+  osc.send({ "localhost", 57120 }, "/nb_monamie/note_on", {voice, freq, vel})
 end
 
 local function note_off(voice)
-  osc.send({ "localhost", 57120 }, "/nb_mesamis/note_off", {voice})
+  osc.send({ "localhost", 57120 }, "/nb_monamie/note_off", {voice})
 end
 
 local function set_param(key, val)
-  osc.send({ "localhost", 57120 }, "/nb_mesamis/set_param", {key, val})
+  osc.send({ "localhost", 57120 }, "/nb_monamie/set_param", {key, val})
 end
 
 
@@ -53,31 +55,31 @@ local function save_synth_patch(txt)
   if txt then
     local patch = {}
     for _, v in ipairs(paramlist) do
-      patch[v] = params:get("nb_mesamis_"..v)
+      patch[v] = params:get("nb_monamie_"..v)
     end
-    tab.save(patch, preset_path.."/"..txt..".pma")
+    tab.save(patch, preset_path.."/"..txt..".pam")
     current_patch = txt
-    print("saved mesamis patch: "..txt)
+    print("saved monamie patch: "..txt)
   end
 end
 
 local function load_synth_patch(path)
   if path ~= "cancel" and path ~= "" then
     dont_panic()
-    if path:match("^.+(%..+)$") == ".pma" then
+    if path:match("^.+(%..+)$") == ".pam" then
       local patch = tab.load(path)
       if patch ~= nil then
         for k, v in pairs(patch) do
-          params:set("nb_mesamis_"..k, v)
+          params:set("nb_monamie_"..k, v)
         end
         local name = path:match("[^/]*$")
-        current_patch = name:gsub(".pma", "")
-        print("loaded mesamis: "..current_patch)
+        current_patch = name:gsub(".pam", "")
+        print("loaded monamie: "..current_patch)
       else
         print("error: could not find patch", path)
       end
     else
-      print("error: not a mesamis patch file")
+      print("error: not a monamie patch file")
     end
   end
 end
@@ -131,7 +133,7 @@ local function fm_index_display(val)
   elseif val > 0.01 then
     str = "linear: "..num
   else
-    str = "0"
+    str = "0%"
   end
   return str
 end
@@ -165,82 +167,82 @@ local function time_display(val)
   return param
 end
 
-local function add_mesamis_params()
-  params:add_group("nb_mesamis_group", "mes amis", 26)
-  params:hide("nb_mesamis_group")
+local function add_monamie_params()
+  params:add_group("nb_monamie_group", "monami.e", 26)
+  params:hide("nb_monamie_group")
 
-  params:add_separator("nb_mesamis_patches", "presets")
+  params:add_separator("nb_monamie_patches", "presets")
 
-  params:add_trigger("nb_mesamis_load", ">> load")
-  params:set_action("nb_mesamis_load", function() fs.enter(preset_path, load_synth_patch) end)
+  params:add_trigger("nb_monamie_load", ">> load")
+  params:set_action("nb_monamie_load", function() fs.enter(preset_path, load_synth_patch) end)
   
-  params:add_trigger("nb_mesamis_save", "<< save")
-  params:set_action("nb_mesamis_save", function() tx.enter(save_synth_patch, current_patch) end)
+  params:add_trigger("nb_monamie_save", "<< save")
+  params:set_action("nb_monamie_save", function() tx.enter(save_synth_patch, current_patch) end)
 
-  params:add_separator("nb_mesamis_levels", "levels")
-  params:add_control("nb_mesamis_level", "level", controlspec.new(0, 1, "lin", 0, 0.8), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_level", function(val) set_param('level', val) end)
+  params:add_separator("nb_monamie_levels", "levels")
+  params:add_control("nb_monamie_level", "level", controlspec.new(0, 1, "lin", 0, 0.8), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_level", function(val) set_param('level', val) end)
 
-  params:add_control("nb_mesamis_pan", "pan", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return pan_display(param:get()) end)
-  params:set_action("nb_mesamis_pan", function(val) set_param('pan', val) end)
+  params:add_control("nb_monamie_pan", "pan", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return pan_display(param:get()) end)
+  params:set_action("nb_monamie_pan", function(val) set_param('pan', val) end)
 
-  params:add_control("nb_mesamis_pan_drift", "pan_drift", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_pan_drift", function(val) set_param('panDrift', val) end)
+  params:add_control("nb_monamie_pan_drift", "pan drift", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_pan_drift", function(val) set_param('panDrift', val) end)
 
-  params:add_control("nb_mesamis_send_a", "send a", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_send_a", function(val) set_param('sendA', val) end)
+  params:add_control("nb_monamie_send_a", "send a", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_send_a", function(val) set_param('sendA', val) end)
   
-  params:add_control("nb_mesamis_send_b", "send b", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_send_b", function(val) set_param('sendB', val) end)
+  params:add_control("nb_monamie_send_b", "send b", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_send_b", function(val) set_param('sendB', val) end)
 
-  params:add_separator("nb_mesamis_sound", "sound")
+  params:add_separator("nb_monamie_sound", "sound")
 
-  params:add_number("nb_mesamis_pitchbend", "pitchbend", 1, 24, 7, function(param) return param:get().."st" end)
-  params:set_action("nb_mesamis_pitchbend", function(val) set_param('pitchBend', val) end)
+  params:add_number("nb_monamie_pitchbend", "pitchbend", 1, 24, 7, function(param) return param:get().."st" end)
+  params:set_action("nb_monamie_pitchbend", function(val) set_param('pitchBend', val) end)
 
-  params:add_control("nb_mesamis_ramp", "ramp", controlspec.new(-1, 1, "lin", 0, 0), function(param) return ramp_display(param:get()) end)
-  params:set_action("nb_mesamis_ramp", function(val) set_param('ramp', val) end)
+  params:add_control("nb_monamie_ramp", "ramp", controlspec.new(-1, 1, "lin", 0, 0), function(param) return ramp_display(param:get()) end)
+  params:set_action("nb_monamie_ramp", function(val) set_param('ramp', val) end)
 
-  params:add_control("nb_mesamis_curve", "curve", controlspec.new(-1, 1, "lin", 0, 0), function(param) return curve_display(param:get())  end)
-  params:set_action("nb_mesamis_curve", function(val) set_param('curve', val) end)
+  params:add_control("nb_monamie_curve", "curve", controlspec.new(-1, 1, "lin", 0, 0), function(param) return curve_display(param:get())  end)
+  params:set_action("nb_monamie_curve", function(val) set_param('curve', val) end)
 
-  params:add_control("nb_mesamis_fm_ratio", "fm ratio", controlspec.new(0.5, 2, "lin", 0, 1, "", 1/150), function(param) return round_form(param:get(), 0.01, "*") end)
-  params:set_action("nb_mesamis_fm_ratio", function(val) set_param('fmRatio', val) end)
+  params:add_control("nb_monamie_fm_ratio", "fm ratio", controlspec.new(0.5, 2, "lin", 0, 1, "", 1/150), function(param) return round_form(param:get(), 0.01, "*") end)
+  params:set_action("nb_monamie_fm_ratio", function(val) set_param('fmRatio', val) end)
 
-  params:add_control("nb_mesamis_fm_index", "fm index", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return fm_index_display(param:get()) end)
-  params:set_action("nb_mesamis_fm_index", function(val) set_param('fmIndex', val) end)
+  params:add_control("nb_monamie_fm_index", "fm index", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return fm_index_display(param:get()) end)
+  params:set_action("nb_monamie_fm_index", function(val) set_param('fmIndex', val) end)
 
-  params:add_separator("nb_mesamis_env", "envelope")
+  params:add_separator("nb_monamie_env", "envelope")
 
-  params:add_option("nb_mesamis_env_mode", "mode", {"AR", "ASR"}, 1)
-  params:set_action("nb_mesamis_env_mode", function(val) set_param('envMode', val - 1) end)
+  params:add_option("nb_monamie_env_mode", "mode", {"AR", "ASR", "CYCLE"}, 1)
+  params:set_action("nb_monamie_env_mode", function(val) set_param('envMode', val - 1) end)
 
-  params:add_control("nb_mesamis_env_slope", "slope [atk/rel]", controlspec.new(0, 1, "lin", 0, 0), function(param) return mix_display(param:get()) end)
-  params:set_action("nb_mesamis_env_slope", function(val) set_param('envRatio', val) end)
+  params:add_control("nb_monamie_env_slope", "slope [atk/rel]", controlspec.new(0, 1, "lin", 0, 0), function(param) return mix_display(param:get()) end)
+  params:set_action("nb_monamie_env_slope", function(val) set_param('envRatio', val) end)
 
-  params:add_control("nb_mesamis_env_time", "time", controlspec.new(0.01, 8, "exp", 0, 0, "", 1/500), function(param) return time_display(param:get()) end)
-  params:set_action("nb_mesamis_env_time", function(val) set_param('envDur', val) end)
+  params:add_control("nb_monamie_env_time", "time", controlspec.new(0.01, 8, "exp", 0, 0, "", 1/500), function(param) return time_display(param:get()) end)
+  params:set_action("nb_monamie_env_time", function(val) set_param('envDur', val) end)
 
-  params:add_separator("nb_mesamis_mod", "modulation")
+  params:add_separator("nb_monamie_mod", "modulation")
 
-  params:add_control("nb_mesamis_morph_amt", "morph amt [map me]", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_morph_amt", function(val) set_param('modDepth', val) end)
-  params:set_save("nb_mesamis_morph_amt", false)
+  params:add_control("nb_monamie_morph_amt", "morph amt [map me]", controlspec.new(0, 1, "lin", 0, 0), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_morph_amt", function(val) set_param('modDepth', val) end)
+  params:set_save("nb_monamie_morph_amt", false)
 
-  params:add_control("nb_mesamis_ramp_mod", "ramp", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_ramp_mod", function(val) set_param('rampMod', val) end)
+  params:add_control("nb_monamie_ramp_mod", "ramp", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_ramp_mod", function(val) set_param('rampMod', val) end)
 
-  params:add_control("nb_mesamis_curve_mod", "curve", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_curve_mod", function(val) set_param('curveMod', val) end)
+  params:add_control("nb_monamie_curve_mod", "curve", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_curve_mod", function(val) set_param('curveMod', val) end)
 
-  params:add_control("nb_mesamis_fm_mod", "fm index", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_fm_mod", function(val) set_param('fmMod', val) end)
+  params:add_control("nb_monamie_fm_mod", "fm index", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_fm_mod", function(val) set_param('fmMod', val) end)
 
-  params:add_control("nb_mesamis_send_a_mod", "send a", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_send_a_mod", function(val) set_param('sendAMod', val) end)
+  params:add_control("nb_monamie_send_a_mod", "send a", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_send_a_mod", function(val) set_param('sendAMod', val) end)
 
-  params:add_control("nb_mesamis_send_b_mod", "send b", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
-  params:set_action("nb_mesamis_send_b_mod", function(val) set_param('sendBMod', val) end)
+  params:add_control("nb_monamie_send_b_mod", "send b", controlspec.new(-1, 1, "lin", 0, 0, "", 1/200), function(param) return round_form(param:get() * 100, 1, "%") end)
+  params:set_action("nb_monamie_send_b_mod", function(val) set_param('sendBMod', val) end)
 
   clock.run(function()
     clock.sleep(0.1)
@@ -251,7 +253,7 @@ end
 
 ---------------- nb player ----------------
 
-function add_nb_mesamis_player()
+function add_nb_monamie_player()
   local player = {
     alloc = vx.new(NUM_VOICES, 2),
     slot = {},
@@ -268,12 +270,12 @@ function add_nb_mesamis_player()
         clock.sleep(0.2)
         if not self.is_active then
           self.is_active = true
-          params:show("nb_mesamis_group")
+          params:show("nb_monamie_group")
           if md.is_loaded("fx") == false then
-            params:hide("nb_mesamis_send_a")
-            params:hide("nb_mesamis_send_b")
-            params:hide("nb_mesamis_send_a_mod")
-            params:hide("nb_mesamis_send_b_mod")
+            params:hide("nb_monamie_send_a")
+            params:hide("nb_monamie_send_b")
+            params:hide("nb_monamie_send_a_mod")
+            params:hide("nb_monamie_send_b_mod")
           end
           _menu.rebuild_params()
         end
@@ -291,7 +293,7 @@ function add_nb_mesamis_player()
         if self.is_active then
           self.is_active = false
           dont_panic()
-          params:hide("nb_mesamis_group")
+          params:hide("nb_monamie_group")
           _menu.rebuild_params()
         end
       end)
@@ -299,11 +301,11 @@ function add_nb_mesamis_player()
   end
 
   function player:stop_all()
-    osc.send({ "localhost", 57120 }, "/nb_mesamis/panic", {})
+    osc.send({ "localhost", 57120 }, "/nb_monamie/panic", {})
   end
 
   function player:modulate(val)
-    params:set("nb_mesamis_morph_amt", val)
+    params:set("nb_monamie_morph_amt", val)
   end
 
   function player:set_slew(s)
@@ -311,7 +313,7 @@ function add_nb_mesamis_player()
 
   function player:describe()
     return {
-      name = "mes amis",
+      name = "monami.e",
       supports_bend = false,
       supports_slew = false
     }
@@ -348,13 +350,13 @@ function add_nb_mesamis_player()
   end
 
   function player:add_params()
-    add_mesamis_params()
+    add_monamie_params()
   end
 
   if note_players == nil then
     note_players = {}
   end
-  note_players["mes amis"] = player
+  note_players["monami.e"] = player
 end
 
 
@@ -363,14 +365,14 @@ end
 local function post_system()
   if util.file_exists(preset_path) == false then
     util.make_dir(preset_path)
-    os.execute('cp '.. '/home/we/dust/code/nb_polyform/data/polyform_patches/*.pfp '.. preset_path)
+    os.execute('cp '.. '/home/we/dust/code/nb_monamie/data/monamie_patches/*.pam '.. preset_path)
   end
 end
 
 function pre_init()
-  init_nb_mesamis()
-  add_nb_mesamis_player()
+  init_nb_monamie()
+  add_nb_monamie_player()
 end
 
-md.hook.register("system_post_startup", "nb mes-amis post startup", post_system)
-md.hook.register("script_pre_init", "nb mes-amis pre init", pre_init)
+md.hook.register("system_post_startup", "nb monami.e post startup", post_system)
+md.hook.register("script_pre_init", "nb monami.e pre init", pre_init)
